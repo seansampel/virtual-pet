9341e97cb88fdf9ec2bedfa3fbb54cec44f0e951
Sean <53915643+sampelmedia@users.noreply.github.com>
2019-11-10 23:22:32 +0000

    add growUp fn

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,14 +1,24 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    
+
     it('sets the name property', () => {
       const pet = new Pet('Fido');
   
       expect(pet.name).toEqual('Fido');
+     
+    });
+
+    describe('growUp', () => {
+      it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+    
+        pet.growUp();
+    
+        expect(pet.age).toEqual(1);
+      });
     });
   });
-
 
 
 
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,29 @@
+const fido = new Pet('Fido');
+const rex = new Pet('Rex');
 
 function Pet(name) {
-    this.name = name;
-  };
+  this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
+  return this;
+};
+Pet.prototype.growUp = function() {
+  this.age += 1;
+  return this;
+};
+rex.growUp = function () {
+  this.age += 5;
+  return this;
+};
+fido.growUp = function () {
+  this.age += 1;
+  return this;
+};
 
-  const pet = {};
 
-  pet.name = 'Fido';
+console.log(rex.growUp()); 
+console.log(fido.growUp()); 
 
-module.exports = Pet;
+  
+  module.exports = Pet;
