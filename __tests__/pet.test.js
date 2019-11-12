875cb428dd1526149b67d939537a8a086e95962e
Sean <53915643+sampelmedia@users.noreply.github.com>
2019-11-12 20:13:19 +0000

    add hunger, fitness, walk

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,24 +1,48 @@
-const Pet = require('../src/pet');
+const Pet = require("../src/pet");
 
-describe('constructor', () => {
+describe("constructor", () => {
+  it("sets the name property", () => {
+    const pet = new Pet("Fido");
 
-    it('sets the name property', () => {
-      const pet = new Pet('Fido');
-  
-      expect(pet.name).toEqual('Fido');
-     
-    });
-
-    describe('growUp', () => {
+    expect(pet.name).toEqual("Fido");
+  });
+});
+/* describe('growUp', () => {
       it('increments the age by 1', () => {
         const pet = new Pet('Fido');
     
         pet.growUp();
     
         expect(pet.age).toEqual(1);
+      }); */
+
+describe("growUp", () => {
+  it("increments the hunger by 5", () => {
+    const pet = new Pet("Fido");
+
+    pet.growUp();
+
+    expect(pet.hunger).toEqual(5);
+  });
+
+  describe("growUp", () => {
+    it("decreases the fitness by 3", () => {
+      const pet = new Pet("Fido");
+
+      pet.growUp();
+
+      expect(pet.fitness).toEqual(7);
+    });
+
+    describe("walk", () => {
+      it("increases fitness by 4", () => {
+        const pet = new Pet("fido");
+
+        pet.fitness = 4;
+        pet.walk();
+
+        expect(pet.fitness).toEqual(8);
       });
     });
   });
-
-
-
+});
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,5 @@
-const fido = new Pet('Fido');
-const rex = new Pet('Rex');
+const fido = new Pet("Fido");
+// const rex = new Pet('Rex');
 
 function Pet(name) {
   this.name = name;
@@ -7,23 +7,33 @@ function Pet(name) {
   this.hunger = 0;
   this.fitness = 10;
   return this;
-};
+}
 Pet.prototype.growUp = function() {
   this.age += 1;
+  this.hunger += 5;
+  this.fitness -= 3;
   return this;
 };
-rex.growUp = function () {
-  this.age += 5;
+// fido.growUp = function () {
+//this.age += 1;
+//  return this;
+//};
+
+Pet.prototype.walk = function() {
+  this.fitness += 4;
   return this;
 };
-fido.growUp = function () {
-  this.age += 1;
-  return this;
-};
+/* if (this.fitness > 3);
+{
+  return "I need a walk";
+}
+if (this.hunger < 5);
+{
+  return "I am hungry";
+}
+if (this.fitness && this.hunger === false);
+return "I feel great!"; */
 
+console.log();
 
-console.log(rex.growUp()); 
-console.log(fido.growUp()); 
-
-  
-  module.exports = Pet;
+module.exports = Pet;
